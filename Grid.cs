288f6d208063a8d0243e35fde4c049f46ad15ced
deaemil <desislava.e.atanasova@outlook.com>
2020-07-24 16:13:53 +0300

    init commit

---
+++ Grid.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lifegame
+{
+    class Grid
+    {
+        private int col;
+        private int row;
+        private int[,] grid;
+        private int x1;
+        private int y1;
+        private int generations;
+        private int counter;
+
+        
+        public Grid()
+        {
+        }
+
+        public Grid(int col, int row, int[,] grid, int x1, int y1, int generations)
+        {
+            this.col = col;
+            this.row = row;
+            this.grid = grid;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.generations = generations;
+        }
+
+        public int GetResult() {
+            int result = 0;
+           
+           int count= MakeNGenerations(grid, generations);
+
+            result = count;
+            return result;
+            
+        }
+
+        public int MakeNGenerations(int[,] firstArr, int n)
+        {
+            int sum = 0;
+            counter = firstArr[y1, x1]; 
+            int[,] secondArr=new int[row,col];
+           //n- generations
+            for(var g=0;g<n;g++)
+            {
+                // current cell
+                for (var k = 0; k < row; k++)
+                {
+
+                    for (var l = 0; l < col; l++)
+                    {
+                        sum = 0;
+                        //around cells
+                        for (var j = k - 1; j <= k + 1; j++)
+                        {
+                            if (j >= 0 && j < row)
+                            {
+                                for (var i = l - 1; i <= l + 1; i++)
+                                {
+                                    if (i >= 0 && i < col)
+                                    {
+                                        sum += firstArr[j, i];
+                                    }
+                                }
+                            }
+                        }
+                        //Checks for cell
+                        if(firstArr[k,l]==1 )
+                        {
+                           sum -= firstArr[k, l];
+                        }
+                        if (sum == 3 || sum == 6 || (firstArr[k, l] == 1 && sum == 2))
+                        {
+                            secondArr[k, l] = 1;
+                        }
+                        else { secondArr[k, l] = 0; }
+
+                        if (k == y1 && l == x1 && secondArr[k, l] == 1)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+
+                int[,] arr = firstArr;
+                firstArr = secondArr;
+                secondArr = arr;             
+
+            }
+
+            return counter;
+        }
+        
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace lifegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Input
+            //1.1. GameBoard size x,y
+            
+            Console.Write("board size:");
+            string s = Console.ReadLine();
+            string[] boardsize = s.Split(",");
+            //width
+            int x = Int32.Parse(boardsize[0]);
+            //height
+            int y = Int32.Parse(boardsize[1]);
+
+            //1.2. Generation Zero
+            Console.WriteLine("Generation Zero:");
+            string[] gridValues= new string[y];
+            for (var i =0; i<y; i++)
+            {
+                gridValues[i] = Console.ReadLine();
+                if (gridValues[i].Length > x|| gridValues[i].Length<x)
+                {
+                    Console.WriteLine("wrong input");
+                    return;
+                }
+            }
+            char[] arrX = new char[x];
+            int[,] gridArray = new int[y,x];
+            for (var i=0; i < y; i++)
+            {          
+                  arrX =gridValues[i].ToCharArray();
+         
+                for(var j = 0; j < x; j++)
+                {
+                    gridArray[i, j] = arrX[j]-'0';
+                }
+            }
+
+            //1.3 Coordinate and n generations
+            Console.Write("Coordinates x1,y1 and number of generations:");
+            string str = Console.ReadLine();
+            string[] input = str.Split(",");
+            int x1 = Int32.Parse(input[0]);
+            int y1 = Int32.Parse(input[1]);
+            int generations = Int32.Parse(input[2]);
+
+            //2. 
+            Grid grid = new Grid(x,y,gridArray,x1,y1,generations);
+
+            //3.Output
+            int result = grid.GetResult();
+            Console.WriteLine(result);
+        }
+    }
+}
